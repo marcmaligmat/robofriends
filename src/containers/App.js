bdcb2c57c5d77375750918d597fd68b6aa3936a3
Marcris Maligmat <marcmaligmat@gmail.com>
2022-05-16 19:54:12 +0800

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react"
+import CardList from "../components/CardList"
+
+import SearchBox from "../components/SearchBox"
+import Scroll from "../components/Scroll"
+import ErrorBoundary from "../components/ErrorBoundary"
+import "./App.css"
+
+const App = () => {
+  const [robots, setRobots] = useState([])
+  const [searchField, setSearchField] = useState("")
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => setRobots(users))
+    console.log(count)
+  }, [count])
+
+  const onSearchChange = event => {
+    setSearchField(event.target.value)
+  }
+
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">Robofriends</h1>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      <SearchBox searchfield={searchField} searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  )
+}
+
+export default App
